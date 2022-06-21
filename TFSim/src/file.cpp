8b11c75ad9f0b4db662feab6e0b374d2cd4fe0cf
Junior <estrutura.pilha@gmail.com>
2022-06-21 19:10:48 -0400

    Alterando o arquivo file.cpp para salvar valores relacionados a metrica

--- TFSim/src/file.cpp
+++ TFSim/src/file.cpp
@@ -2,12 +2,50 @@
 
 FileOut::FileOut(){
     try{
-        string path = string(get_current_dir_name()) + "/out/saida.txt";
+        string path = string(get_current_dir_name()) + "/out/experimenter.csv";
+        ifstream f(path);
+        bool fileExist = true;
+        if(!f){
+            fileExist = false;
+        }
         file.open(path, std::ios_base::app);
+        if(!fileExist){
+            file << "data,programa,n_instrucao,n_clock,cpi,ipc,mips,t_cpu" << endl;
+        }
     }catch(...){
         cout << "\nErro ao abrir o arquivo\n";
     }
 }
+
+void FileOut::add_program(string attribute){
+    //attributes[0] = attribute;
+     file << __TIMESTAMP__ << "," << attribute << ",";
+}
+void FileOut::add_clock(string attribute){
+    //attributes[2] = attribute;
+    file << attribute << ",";
+}
+void FileOut::add_n_instruction(string attribute){
+    //attributes[1] = attribute;
+    file  << attribute << ",";
+}
+void FileOut::add_cpi(string attribute){
+    file << attribute;
+}
+
+void FileOut::save_file(){
+    try{
+       /* file << __TIMESTAMP__ ;
+        for(int i = 0; i < 3; i++){
+            file << "," << attributes[i];
+        } */
+        file << endl;
+    }catch(...){
+        cout << "\n Erro ao salvar no arquivo \n";
+    }
+}
+
+
 
 void FileOut::add_str(string line){
     try{
